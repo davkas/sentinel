fa5b083e35fc96d8104589555151799626b3b4d3
davkas <davkas@davkasdeMacBook-Pro.local>
2015-12-06 01:25:25 +0800

    temp

--- sentinel_storm/src/main/java/com/davkas/sentinel/storm/topology/topology.java
+++ sentinel_storm/src/main/java/com/davkas/sentinel/storm/topology/topology.java
@@ -1,11 +1,14 @@
 package com.davkas.sentinel.storm.topology;
 
+import backtype.storm.topology.TopologyBuilder;
+
 /**
  * Created by davkas on 15/12/6.
  */
 public class topology {
 
     public static void main(String[] args) {
-
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+        
     }
 }
