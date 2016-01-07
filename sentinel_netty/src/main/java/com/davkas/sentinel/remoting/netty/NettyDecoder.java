4193193771945de91fc2dc51aca835a520f6b670
davkas <davkas@davkas.local>
2016-01-07 22:12:56 +0800

    netty remoting

--- sentinel_netty/src/main/java/com/davkas/sentinel/remoting/netty/NettyDecoder.java
+++ sentinel_netty/src/main/java/com/davkas/sentinel/remoting/netty/NettyDecoder.java
@@ -1,7 +1,37 @@
 package com.davkas.sentinel.remoting.netty;
 
+import com.davkas.sentinel.remoting.protocol.RemotingCommand;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created by hzzhengxianrui on 2015/12/27.
  */
-public class NettyDecoder {
+public class NettyDecoder extends LengthFieldBasedFrameDecoder{
+
+    private static final int FRAME_MAX_LENGTH = //
+            Integer.parseInt(System.getProperty("com.rocketmq.remoting.frameMaxLength", "8388608"));
+
+
+    public NettyDecoder(){
+        super(FRAME_MAX_LENGTH,0,4,0,4);
+    }
+
+    @Override
+    protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        ByteBuf frame = null;
+        frame = (ByteBuf) super.decode(ctx, in);
+        if (null == frame) {
+            return null;
+        }
+
+        ByteBuffer byteBuffer = frame.nioBuffer();
+        return RemotingCommand.
+
+
+
+    }
 }
--- sentinel_netty/src/main/java/com/davkas/sentinel/remoting/protocol/RemotingCommand.java
+++ sentinel_netty/src/main/java/com/davkas/sentinel/remoting/protocol/RemotingCommand.java
@@ -4,5 +4,6 @@ package com.davkas.sentinel.remoting.protocol;
  * Created by hzzhengxianrui on 2015/12/27.
  */
 public class RemotingCommand {
-    
+   
+
 }
